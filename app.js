9d452e9c15d955d3d31a989f1417c6ef9d6a1f9f
anushkagarg5653 <anushkagarg0811@gmail.com>
2021-06-01 15:01:27 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,2 +1,32 @@
-var anu =  prompt["give me your username"]
-alert("NOOOOOOOOOOOO" + anu);
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+
+function errorHandler(error) {
+    log("error occured" + error);
+    alert("Something is wrong with server! Try again after sometime!")
+}
+
+
+function clickHandler() {
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+        .then(Response => Response.json())
+        .then(json => {
+            var translatedText = json.contents.translated
+            outputDiv.innerText = translatedText
+        })
+        .catch(errorHandler)
+
+}
+
+function getTranslationURL(inputText) {
+    return serverURL + "?" + "text=" + inputText;
+}
+
+
+btnTranslate.addEventListener("click", clickHandler);
